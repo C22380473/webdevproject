226220223e00fdab22cabb53e660581c9f52fb9c
Wissam Hadjarab <C21404706@mytudublin.ie>
2024-11-20 16:12:54 +0000

    adding users.js

--- model/db.js
+++ model/db.js
@@ -1,4 +1,4 @@
-db.js:
+
 
 const mongoose = require('mongoose');
 
@@ -11,20 +11,3 @@ mongoose.connect('mongodb://localhost:27017/recipeApp', {
   .catch(err => console.error('Error connecting to MongoDB:', err));
 
 module.exports = mongoose; // Export mongoose for use in other files
-/*var mysql = require('mysql2');
-
-	var con = mysql.createConnection({
-	  host: "localhost",
-	  user: "root",
-	  password: "qwerty",
-	  database: "mydb",
-	  port:3306
-	});
-
-	con.connect(function(err) {
-	  if (err) throw err;
-		console.log("Connected to MySQL Database")	
-	});
-
-	module.exports = con;
-*/
---
+++ model/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }, // Store hashed passwords
+});
+
+// Before saving the user, hash the password
+userSchema.pre('save', function(next) {
+    if (!this.isModified('password')) return next();
+  
+    bcrypt.hash(this.password, 10, (err, hashedPassword) => {
+      if (err) return next(err);
+      this.password = hashedPassword;
+      next();
+    });
+  });
+  
+  const User = mongoose.model('User', userSchema); // Create the model
+  
+  module.exports = User; // Export it to use in your server
+  
