723dcc2b58b71aee48a3f969adce0a14bb1ee9da
Jen Sayago <Jen@Jens-MacBook-Pro.local>
2024-11-20 15:54:17 +0000

    server updated

--- model/db.js
+++ model/db.js
@@ -3,7 +3,7 @@ var mysql = require('mysql2');
 	var con = mysql.createConnection({
 	  host: "localhost",
 	  user: "root",
-	  password: "123456",
+	  password: "qwerty",
 	  database: "mydb",
 	  port:3306
 	});
